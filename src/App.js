67503110860b4635e45f4ad96749eec3cafee873
Samantha Gatt <smfgatt@gmail.com>
2020-05-04 14:25:41 -0400

    Extracts App into its own component file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Title from './components/Title';
+import DragonList from './components/DragonList';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <DragonList />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import logger from "redux-logger";
 
 import { rootReducer } from "./reducers";
 
-import Title from './components/Title';
-import DragonList from './components/DragonList';
-import './styles.css';
+import App from "./App";
 
-const store = createStore(rootReducer);
-
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <DragonList />
-    </div>
-  );
-}
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
